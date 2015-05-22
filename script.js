84bf323f52d16a8307f49d17b19d5cf78ae630f8
dmingust <dmingust@gmail.com>
2015-05-21 21:19:25 -0500

    update README

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
-	for (var i = 0; i < 16; i++) {
-		$(".wrapper").append('<div class="gridBox"></div>');
-	};
+	//for (var i = 0; i < 16; i++) {
+	//	$(".wrapper").append('<div class="gridBox"></div>');
+	//};
+	n = 16;
+	createGrid(n);
+	createTrail();
+
+});
+
+
+function createTrail(){
 	$('.gridBox').hover(
 		function() {
 			$(this).addClass('on');
@@ -10,5 +18,26 @@ $(document).ready(function(){
 			$(this).addClass('off');
 		}
 		);
+}
+
+function clearBoard() {
+	$('.gridBox').removeClass('on');
+	$('.gridBox').removeClass('off');
+	$("tr").remove();
 	
-});
+	numSides = prompt("How many squares per side would you like?");
+	createGrid(numSides);
+	createTrail();
+}
+
+function createGrid(size){
+
+	for(i = 0; i < size; i++){
+		$tr = $('<tr></tr>');
+		for(j = 0; j < size; j++){
+			$div = $("<div class='gridBox'></div>");
+			$tr.append($("<td></td>").append($div));
+		}
+		$(".wrapper").append($tr);
+	}
+}
